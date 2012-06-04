e0accad2f4f7864681ae60097bc2e61ce2e304ac
Sylvain Boucher <sylvainboucher99@gmail.com>
2012-06-04 16:54:56 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,117 @@
+<?php
+
+require 'Slim/Slim.php';
+
+$app = new Slim();
+
+$app->get('/users', 'getUsers');
+$app->get('/users/:id',	'getUser');
+$app->get('/users/search/:query', 'findByName');
+$app->post('/users', 'addUser');
+$app->put('/users/:id', 'updateUser');
+$app->delete('/users/:id',	'deleteUser');
+
+$app->run();
+
+function getUsers() {
+	$sql = "SELECT * FROM users ORDER BY name";
+	try {
+		$db = getConnection();
+		$stmt = $db->query($sql);  
+		$users = $stmt->fetchAll(PDO::FETCH_OBJ);
+		$db = null;
+		echo json_encode($users);
+	} catch(PDOException $e) {
+
+	}
+}
+
+function getUser($id) {
+	$sql = "SELECT * FROM users WHERE id=:id";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);  
+		$stmt->bindParam("id", $id);
+		$stmt->execute();
+		$user = $stmt->fetchObject();  
+		$db = null;
+		echo json_encode($user); 
+	} catch(PDOException $e) {
+
+	}
+}
+
+function addUser() {
+	$request = Slim::getInstance()->request();
+	$user = json_decode($request->getBody());
+	$sql = "INSERT INTO users (name) VALUES (:name)";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);  
+		$stmt->bindParam("name", $user->name);
+		$stmt->execute();
+		$user->id = $db->lastInsertId();
+		$db = null;
+		echo json_encode($user); 
+	} catch(PDOException $e) {
+		
+	}
+}
+
+function updateUser($id) {
+	$request = Slim::getInstance()->request();
+	$body = $request->getBody();
+	$user = json_decode($body);
+	$sql = "UPDATE users SET name=:name WHERE id=:id";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);  
+		$stmt->bindParam("name", $user->name);
+		$stmt->execute();
+		$db = null;
+		echo json_encode($user); 
+	} catch(PDOException $e) {
+
+	}
+}
+
+function deleteUser($id) {
+	$sql = "DELETE FROM users WHERE id=:id";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);  
+		$stmt->bindParam("id", $id);
+		$stmt->execute();
+		$db = null;
+	} catch(PDOException $e) {
+
+	}
+}
+
+function findByName($query) {
+	$sql = "SELECT * FROM users WHERE UPPER(name) LIKE :query ORDER BY name";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);
+		$query = "%".$query."%";  
+		$stmt->bindParam("query", $query);
+		$stmt->execute();
+		$user = $stmt->fetchAll(PDO::FETCH_OBJ);
+		$db = null;
+		echo json_encode($user);
+	} catch(PDOException $e) {
+
+	}
+}
+
+function getConnection() {
+	$dbhost="127.0.0.1";
+	$dbuser="root";
+	$dbpass="";
+	$dbname="users";
+	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $dbh;
+}
+
+?>
